b5bc1cebf21b9d0e1e8f5dec97dcc20719786ee8
Yeswanth Reddy <yeswanth.reddy@almonds.ai>
2025-01-15 13:13:58 +0530

    CORS enabled all traffic

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app's URL
+    allow_origins=["*"],  # React app's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
